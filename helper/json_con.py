5a33d54866a6b4251d1126a0efbae2442afcd96a
smlblbn <smlblbn@gmail.com>
2018-05-22 11:25:34 +0300

    project changed

--- helper/json_con.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-with open('general/matches_2017-2018.json') as rounds_json:
-	rounds = pd.read_json(rounds_json)
-
-print(rounds)
-rounds = np.asarray(rounds)
-print(rounds[:,11])
